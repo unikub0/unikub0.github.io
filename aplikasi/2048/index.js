68c4293972f8e601ab79a50b50c238a8eb4ec8ce
UniKub <emmirfragrans@gmail.com>
2025-03-24 13:35:15 +0700

    updated layout, ui + light-pastel/dark-neonic theme

---
+++ aplikasi/2048/index.js
@@ -0,0 +1,10 @@
+function tes() {
+    prompt("Tes")
+}
+function generate() { }
+onload = () => {
+    const container = document.getElementById("container2048")
+    for (let i = 0; i < 16; ++i) {
+
+    }
+}
